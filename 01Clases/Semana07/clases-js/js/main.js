83356c3f20a5a11c672b7ce340e0d713823af720
GiseMontania <129598438+GiseMontania@users.noreply.github.com>
2023-05-12 21:09:46 -0300

    Add files via upload

---
+++ 01Clases/Semana07/clases-js/js/main.js
@@ -0,0 +1,26 @@
+console.log('Hola mundo!');
+
+let num1= 50;
+let num2= 10;
+let saludo= 'Hola!';
+let nombreCurso= 'Código LATAM';
+
+let resultadoSuma= num1+num2;
+let resultadoResta= num1-num2;
+let resultadoMultiplicacion= num1*num2;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + ' Esto es ' + nombreCurso); 
+
+let edadMore= 25;
+let edadNahue= 32;
+let diferenciaEdad= edadNahue - edadMore;
+
+let nombre1= 'More';
+let nombre2= 'Nahue';
+let parentesco= 'hermanos';
+
+console.log(diferenciaEdad);
+console.log(nombre1 + ' y ' + nombre2 + ' son ' + parentesco + '. Se llevan ' + diferenciaEdad + ' años de diferencia de edad ');
